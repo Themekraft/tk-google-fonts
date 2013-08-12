3447130d1eb39461ec25312df9289e8cef18a0d2
themekraft <team@themekraft.com>
2013-08-12 20:34:58 +0200

    Fixed all inline documentation and some cleanup. Ready to Go. ;)

--- js/theme-customize.js
+++ js/theme-customize.js
@@ -1,13 +1,7 @@
-
-/**
- * This file adds some LIVE to the Theme Customizer live preview. To leverage
- * this, set your custom settings to 'postMessage' and then add your handling
- * here. Your javascript should grab settings from customizer controls, and 
- * then make any necessary changes to the page using jQuery.
- */
+// This file here handles the live changes in your preview. 
 ( function( $ ) {
 
-	// Update the site title in real time...
+	// Update the h1 titles in real time...
 	wp.customize( 'h1_font', function( value ) {
 		value.bind( function( newval ) {
 			if(newval == 'none'){
