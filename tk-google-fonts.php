7a24b11f101112de6235ac0ec7ba615f4e1dcd2a
Camilo Luna <camilo@alunizar.co>
2026-05-04 22:02:26 -0700

    build(beta): version 2.3.0-beta.3
    Bumps pricing-page submodule for the new CSS grid card layout.

--- tk-google-fonts.php
+++ tk-google-fonts.php
@@ -3,7 +3,7 @@
  * Plugin Name: TK Google Fonts
  * Plugin URI:  http://themekraft.com/shop/product-category/themes/extentions/
  * Description: Google Fonts UI for WordPress Themes
- * Version: 2.3.0-beta.2
+ * Version: 2.3.0-beta.3
  * Author: ThemeKraft
  * Author URI: http://themekraft.com/
  * Licence: GPLv3
